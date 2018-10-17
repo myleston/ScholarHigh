50cd548520e87137133de66c85718320774115a7
铭元 <DevilCat@mylestons-macbook-pro.local>
2018-10-17 10:26:37 +0900

    Initial Commit

---
+++ ScholarHigh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScholarHigh
+//
+//  Created by Myleston Law on 2018/10/17.
+//  Copyright © 2018 Myleston Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
